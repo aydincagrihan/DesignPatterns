20cd550e815b6642fa550e9f72abd71f1a97ca5c
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2025-08-04 16:06:40 +0300

    1.designpatternnedir

--- 1.Introduction/Program.cs
+++ 1.Introduction/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ 
+ Tasarım Desenleri (Design Patterns) Nedir?
+Yazılım geliştirmede sıkça karşılaşılan, birbirine benzer sorunlara karşı geliştirilmiş, tekrar kullanılabilir ve kanıtlanmış çözüm şablonlarıdır.
+
+Bunlar, doğrudan kopyalanıp yapıştırılan hazır kod parçaları değil, bir problemin nasıl çözüleceğini anlatan birer "tarif" veya "plan" gibidir.
+
+Nereden Ortaya Çıkmıştır?
+Kavram ilk olarak mimar Christopher Alexander tarafından şehir ve bina tasarımlarındaki ortak sorunlara çözüm bulmak amacıyla ortaya atılmıştır.
+
+Yazılım dünyasında ise 1994 yılında Erich Gamma, Richard Helm, Ralph Johnson ve John Vlissides'in (namıdiğer "Dörtlü Çete" - Gang of Four, GoF) yazdığı "Design Patterns: Elements of Reusable Object-Oriented Software" kitabı ile popüler hale gelmiştir. Bu kitap, yazılım dünyası için temel başvuru kaynağıdır.
+
+Amacı Nedir?
+Temel amacı, yazılan kodun daha esnek, anlaşılır, tekrar kullanılabilir ve sürdürülebilir olmasını sağlamaktır. Başlıca hedefleri şunlardır:
+
+Tekrar Kullanılabilirlik: Aynı tür problemler için "tekerleği yeniden icat etmeyi" önler.
+
+Ortak Bir Dil: Geliştiricilerin, karmaşık tasarım fikirlerini "Singleton", "Factory" veya "Observer" gibi ortak ve bilinen isimlerle kolayca tartışmasını sağlar.
+
+Esneklik ve Bakım Kolaylığı: Kodun gelecekteki değişikliklere daha kolay uyum sağlamasına olanak tanır ve bakımını kolaylaştırır.
+
+Kanıtlanmış Çözümler: Yıllar içinde test edilmiş ve en iyi pratikler olarak kabul edilmiş çözümler sunarak geliştirme sürecini hızlandırır ve hataları azaltır.
+ 
+ */
